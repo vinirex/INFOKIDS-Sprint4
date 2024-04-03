41880e76b2fcd0d197f677b978f1fc4ed5529c65
Vinicius Silva <106930436+vinirex@users.noreply.github.com>
2024-04-02 22:34:38 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# SPRINT3-web
+# projeto criando usando react +vite
+
+# O projeto vai sem a pasta node_modules portanto é preciso 
+# abrir o projeto no vscode e no terminal digitar 
+npm install
+
+depois digitar
+
+npm run dev
+
---
+++ src/components/Nav.jsx
@@ -0,0 +1,36 @@
+import { } from 'react';
+import { Link } from 'react-router-dom';
+import '../css/estilo.css';
+import Produto from '../assets/produto.png'
+
+function Nav() {
+  return (
+    <>
+      <header className="menu">
+        <nav className="topnav" id="myTopnav">
+          <div className="topnav" id="myTopnav">
+            <a className="navbar-brand disabled" aria-disabled="true">
+            <img src={Produto}/></a>
+            <Link to="/" className="tlink">
+              Home
+            </Link>{' '}
+            {''}
+            <Link to="/sobre" className="tlink">
+              Sobre
+            </Link>
+            <Link to="/portifolio" className="tlink">
+              Portifólio
+            </Link>
+            <Link to="/contato" className="tlink">
+              Contato
+            </Link>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+              <i class="fa fa-bars"></i>
+            </a>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+}
+export default Nav;
